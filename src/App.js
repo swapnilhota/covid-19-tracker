a4d1b601e9ed839d2c0299d33979b10349e8f250
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-10 10:25:56 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+
+import { FormControl, MenuItem, Select } from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      App
+      <h1>COVID-19 TRACKER</h1>
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">option 2</MenuItem>
+          <MenuItem value="worldwide">option 3</MenuItem>
+          <MenuItem value="worldwide">option 4</MenuItem>
+        </Select>
+      </FormControl>
     </div>
   );
 }
