48807675d31cf96a2fa41d92b8513c4c1616796b
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-10 10:28:58 +0530

    style update

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>COVID-19 TRACKER</h1>
-      <FormControl className="app__dropdown">
-        <Select variant="outlined" value="abc">
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">option 2</MenuItem>
-          <MenuItem value="worldwide">option 3</MenuItem>
-          <MenuItem value="worldwide">option 4</MenuItem>
-        </Select>
-      </FormControl>
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">option 2</MenuItem>
+            <MenuItem value="worldwide">option 3</MenuItem>
+            <MenuItem value="worldwide">option 4</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
