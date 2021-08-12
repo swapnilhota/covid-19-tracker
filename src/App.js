570e9ba3b316bfb84cf31ec9c6d933960fa75417
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-12 18:05:14 +0530

    update

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     getCountriesData();
   }, []);
 
-  const onCountryChange = (e) => {
+  const onCountryChange = async (e) => {
     const countryCode = e.target.value;
 
     const url = countryCode === 'worldwide' ? 'https://disease.sh/v3/covid-19/all' : `https://disease.sh/v3/covid-19/countries/${countryCode}`
