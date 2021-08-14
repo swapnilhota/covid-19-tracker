6101a00be2553285f39ac4e893d1c80d75ac3833
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-14 11:08:07 +0530

    update:

--- src/App.js
+++ src/App.js
@@ -136,8 +136,8 @@ function App() {
         <CardContent>
           <h3>Live Cases by Country</h3>
           <Table countries={tableData} />
-          <h3>Worldwide new cases</h3>
-          <LineGraph />
+          <h3 className="app__graphTitle">Worldwide new cases</h3>
+          <LineGraph className="app__graph" />
         </CardContent>
       </Card>
     </div>
